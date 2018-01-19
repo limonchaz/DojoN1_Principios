c00a171bdf5f59060b05f4c2d55c86cf47d223cf
Francisco Limon <flimon@dsindigo.mx>
2018-01-19 10:16:25 -0600

    Integracion de casos de prueba faltantes resolves
    Se agregaron casos de pruebas faltantes con escenarios del requerimiento

--- kataCodeSmell/src/test/java/com/gildedrose/GildedRoseTest.java
+++ kataCodeSmell/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,39 +7,101 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-   // Item[] items = new Item[]{ new Item("Sulfuras, Hand of Ragnaros", 1, 1) };
-    Item[] items = new Item[] {
-            new Item("+5 Dexterity Vest", 10, 20), //
-            new Item("Aged Brie", 2, 0), //
-            new Item("Elixir of the Mongoose", 5, 7), //
-            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-            new Item("Conjured Mana Cake", 3, 6) };
+    Item[] items = new Item[]{ new Item("Sulfuras, Hand of Ragnaros", 1, 1) };
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("fixme", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
-
-    @Test
-    public void drecrement() {
-        GildedRose app = new GildedRose(items);
+    public void drecrementByOneBefore() {
+        Item[] items = new Item[] { new Item("fixme", 10, 15) };
         
-        for (Item item : items) {
-            int sellInPrev = item.sellIn - 1;
-            int quealityPrev = item.quality - 1;
-            
-            app.drecrementQualitySellInByOne(item);
-            
-            assertEquals(sellInPrev, item.sellIn);
-            assertEquals(quealityPrev, item.quality);
-        }
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        
+        assertEquals(9, items[0].sellIn);
+        assertEquals(14, items[0].quality);
+    }
+    
+    @Test
+    public void drecrementTwice() {
+        Item[] items = new Item[] { new Item("fixme", -1, 15) };
+        
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        
+        assertEquals(-2, items[0].sellIn);
+        assertEquals(13, items[0].quality);
+    }
+    
+    @Test
+    public void qualityNotNegative() {
+        Item[] items = new Item[] { new Item("fixme", 1, 0) };
+        
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        
+        assertEquals(0, items[0].quality);
+    }
+    
+    @Test
+    public void agedBrieQuality() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 2, 1),
+                new Item("Aged Brie", -3, 5)
+        };
+        
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        
+        assertEquals(2, items[0].quality);
+        assertEquals(7, items[1].quality);
+    }
+    
+    @Test
+    public void qualityNeverMoreThanFifty() {
+        Item[] items = new Item[] { 
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)
+        };
+        
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        
+        assertEquals(50, items[0].quality);
+    }
+    
+    @Test
+    public void sulfurasNerverBeSoldOrDecrease() {
+        Item[] items = new Item[] { 
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80)
+        };
+        
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        
+        assertEquals(80, items[0].quality);
+        assertEquals(80, items[1].quality);
+    }
+    
+    @Test
+    public void backstagePassesQuality() {
+        Item[] items = new Item[] { 
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 6),
+                new Item("Backstage passes to a TAFKAL80ETC concert", -2, 10),
+        };
+        
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        
+        assertEquals(32, items[0].quality);
+        assertEquals(9, items[1].quality);
+        assertEquals(0, items[2].quality);
     }
     
     @Test
